b14b23cef91874325f3be2bab984c465ce598c89
penneytech <99091373+penneytech@users.noreply.github.com>
2023-05-03 13:32:27 -0400

    Added client scoring code (manually by mr. penney)

--- server.js
+++ server.js
@@ -14,55 +14,68 @@ app.use(cors());
 app.use(express.static('public'));
 
 // Our Imports
-const clientConnect = require('./client/clientConnect.js'); 
+const clientConnect = require('./client/clientConnect.js');
 const clientIdentify = require('./client/clientIdentify.js')
-const clientLogin = require('./client/clientLogin.js'); 
-const clientMessage = require('./client/clientMessage.js'); 
-const clientDisconnect = require('./client/clientDisconnect.js'); 
+const clientLogin = require('./client/clientLogin.js');
+const clientMessage = require('./client/clientMessage.js');
+const clientDisconnect = require('./client/clientDisconnect.js');
 const leaderBoard = require('./userData/leaderboardPosition.js');
+const clientScore = require('./client/clientScore.js');
+const scoreAdd = require('./client/scoreAdd.js');
 
 let intervalID;
 
 io.on('connection', (socket) => {
 
-    // Handle Client Connections
-    clientConnect(socket);
+  // Handle Client Connections
+  clientConnect(socket);
 
-       // Handle Client Messages
-    socket.on('ident', (message) => {
+  // Handle Client Messages
+  socket.on('ident', (message) => {
     clientIdentify(message, socket, io)
-    });
+  });
 
-    socket.on('login', (message) => {
+  socket.on('login', (message) => {
     clientLogin(message, socket, io)
-    });
-    
-    // Handle Client Messages
-    socket.on('message', (message) => {
-    clientMessage(message, socket, io)
-    });
-    
-    // Handle Client Disconnections
-    socket.on('disconnect', () => {
-    clientDisconnect(socket, io);
-    });   
+  });
 
-        // Start sending test messages to all clients in the 'users' room
-    if (!intervalID) {
-        intervalID = setInterval(() => {
-            //console.log("Test message sent to users")
-            io.to('user').emit(
-                'message', 
-                'This is a test message from the server!');
-        }, 10000);
-    }
-    
+  // Handle Client Messages
+  socket.on('message', (message) => {
+    clientMessage(message, socket, io)
+  });
+
+  // Handle point collection
+  socket.on('collectPoints', (message) => {
+    clientScore(message, socket, io);
+  });
+
+  // Handle End of Round
+  socket.on('roundEnd', () => {
+    scoreAdd(); 3
+  })
+
+  // Handle Client Disconnections
+  socket.on('disconnect', () => {
+    clientDisconnect(socket, io);
+    scoreAdd();
+  });
+
+  // Start sending test messages to all clients in the 'users' room
+  if (!intervalID) {
+    intervalID = setInterval(() => {
+      //console.log("Test message sent to users")
+      io.to('user').emit(
+        'message',
+        'This is a test message from the server!');
+    }, 10000);
+  }
+
 });
 
 server.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 
-    
+
 });
 
-leaderBoard;
+leaderBoard();
