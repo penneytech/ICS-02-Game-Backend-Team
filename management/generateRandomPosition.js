21e93914c0c45dd5f5f26158fe3c0994ff11ebf5
meems07 <130686876+meems07@users.noreply.github.com>
2023-05-17 16:30:46 +0000

    Gems spawning in the same position fixed

--- management/generateRandomPosition.js
+++ management/generateRandomPosition.js
@@ -1,24 +1,31 @@
 const wallsfloor = require('./wallsfloor.js');
 
-function generateRandomPosition(tileWidth, tileHeight) {
+function generateRandomPosition(tileWidth, tileHeight, occupiedPositions) {
     // Get the width of the map in tiles
     const mapWidth = 40;
-  
+
     // Get the indexes of tiles labelled as 10
     const tileIndexes = wallsfloor.flatMap((value, index) => value === 10 ? index : []);
     // If no tiles with label 10 exist, return null
     if (tileIndexes.length === 0) {
-      return null;
+        return null;
     }
   
-    // Randomly select one of the indexes
-    const randomIndex = tileIndexes[Math.floor(Math.random() * tileIndexes.length)];
+    while (true) {
+        // Randomly select one of the indexes
+        const randomIndex = tileIndexes[Math.floor(Math.random() * tileIndexes.length)];
 
-    // Convert the 1D index to 2D coordinates
-    const x = Math.floor((randomIndex % mapWidth) * tileWidth);
-    const y = Math.floor(randomIndex / mapWidth) * tileHeight;
-  
-    return { x, y };
-  }
+        // Convert the 1D index to 2D coordinates
+        const x = Math.floor((randomIndex % mapWidth) * tileWidth);
+        const y = Math.floor(randomIndex / mapWidth) * tileHeight;
+
+        // Check if the position is already occupied
+        if (!occupiedPositions.some(pos => pos.x === x && pos.y === y)) {
+            return { x, y };
+        } else {
+            console.log("POSITION ALREADY TAKEN");
+        }
+    }
+}
 
 module.exports = generateRandomPosition;
--- management/generateTreasure.js
+++ management/generateTreasure.js
@@ -2,27 +2,32 @@ const globals = require('../globals.js');
 const generateRandomPosition = require('./generateRandomPosition.js');
 
 function generateTreasure(arrayLength) {
-const gems = [
-{ name: "Amber", value: 10 },
-{ name: "Ruby", value: 20 },
-{ name: "Sapphire", value: 40 }
-];
+    
+  const gems = [
+    { name: "Amber", value: 10 },
+    { name: "Ruby", value: 20 },
+    { name: "Sapphire", value: 40 }
+  ];
 
-const result = [];
+  const occupiedPositions = [];
 
-for (let i = 0; i < arrayLength; i++) {
-const randomxy = generateRandomPosition(32, 32);
-const randomX = randomxy.x;
-const randomY = randomxy.y;
-const gemIndex = Math.floor(Math.random() * gems.length);
-const gem = gems[gemIndex];
-const treasure = { x: randomX, y: randomY, gem: gem.name, value: gem.value };
-result.push(treasure);
+  const result = [];
+
+  for (let i = 0; i < arrayLength; i++) {
+    const randomxy = generateRandomPosition(32, 32, occupiedPositions);
+    const randomX = randomxy.x;
+    const randomY = randomxy.y;
+    occupiedPositions.push(randomxy);
+    const gemIndex = Math.floor(Math.random() * gems.length);
+    const gem = gems[gemIndex];
+    const treasure = { x: randomX, y: randomY, gem: gem.name, value: gem.value };
+    result.push(treasure);
+  }
+
+  globals.setGlobal('treasure', result);  
+    console.log(result)
 }
 
-return result;
-}
+generateTreasure(50);  // Generate 50 new treasures and save the result to the 'treasure' global variable
 
-const result = generateTreasure(50);
-
-globals.setGlobal('treasure', result);
+//
