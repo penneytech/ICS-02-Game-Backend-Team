68f7702542600702b123d9707829ffd02736a413
penneytech <99091373+penneytech@users.noreply.github.com>
2023-05-19 16:25:16 +0000

    Update timer

--- rounds/roundTimer.js
+++ rounds/roundTimer.js
@@ -4,27 +4,27 @@ let paused = false;
 let globals = require('../globals.js');
 
 function startTimer() {
-    
+
     setInterval(function() {
-      let io = globals.getGlobal('io');
+        let io = globals.getGlobal('io');
 
         if (timeremaining == 0 && paused == false) {
             paused = true; // We're paused
             timeremaining = 5000;
             console.log("PAUSED")
-            io.emit("timerPaused"); // Paused frontend
+            io.emit("timerpaused", paused); // Paused frontend
         }
 
         if (timeremaining == 0 && paused == true) {
             paused = false; // We're playing
-            timeremaining = 10000;                                         
-             console.log("PLAYING");
-            io.emit("timerStarted") // Start frontend
+            timeremaining = 10000;
+            console.log("PLAYING");
+            io.emit("timerpaused", paused); // Paused frontend
         }
 
         timeremaining = timeremaining - 1000;
         globals.setGlobal('timeleft', timeremaining);
-       // console.log(timeremaining);
+        // console.log(timeremaining);
     }, 1000);
 }
 
