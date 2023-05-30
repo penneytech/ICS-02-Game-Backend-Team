147d3b64225a9a6d74582c533fe041cf6e802394
penneytech <99091373+penneytech@users.noreply.github.com>
2023-05-30 10:20:20 -0400

    Update

--- playerPosition/sendPositions.js
+++ playerPosition/sendPositions.js
@@ -4,14 +4,6 @@ function sendPositions(socket, io) {
 
   let connectedclients = globals.getGlobal('connectedclients');
 
-/*  const clientIndex = connectedclients.findIndex(client => client.id === socket.id);
-
-  io.emit('sendPosition', {
-      username: connectedclients[clientIndex].username,
-      xPosition: connectedclients[clientIndex].xPosition,
-      yPosition: connectedclients[clientIndex].yPosition
-  });*/
-  
   const positions = connectedclients
     .filter(client => client.username !== "")
     .map(client => ({
@@ -19,7 +11,9 @@ function sendPositions(socket, io) {
       xPosition: client.xPosition,
       yPosition: client.yPosition
     }));
-socket.emit('sendPositions', positions);
+
+  socket.emit('sendPositions', positions);
+
 }
 
 module.exports = sendPositions;
